151118a3f3ef9afa3ed3283e64209586f8cf2bfd
Shawn de Bie <shawn.debie@gamil.com>
2026-02-18 11:34:34 +0200

    fixed order id

--- src/main/java/org/ecommerce/persistance/dto/OrderDto.java
+++ src/main/java/org/ecommerce/persistance/dto/OrderDto.java
@@ -7,11 +7,12 @@ import java.util.List;
 
 @Data
 public class OrderDto {
-    long orderId;
+    // Use wrapper type Long so GraphQL input can be nullable during create
+    Long orderId;
     List<OrderItemEntity> items;
 
     // Explicit getters to avoid reliance on Lombok during build
-    public long getOrderId() {
+    public Long getOrderId() {
         return orderId;
     }
 
