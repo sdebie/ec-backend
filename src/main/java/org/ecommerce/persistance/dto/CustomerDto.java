40d8fde00678246bf439396df266b3830329ed6c
Shawn de Bie <shawn.debie@gamil.com>
2026-02-19 09:58:33 +0200

    added email getter

--- src/main/java/org/ecommerce/persistance/dto/CustomerDto.java
+++ src/main/java/org/ecommerce/persistance/dto/CustomerDto.java
@@ -1,12 +1,17 @@
 package org.ecommerce.persistance.dto;
 
-import lombok.Data;
-
 /**
  * Simple DTO for customer information coming from the frontend.
  * For now it only contains the email address.
  */
-@Data
 public class CustomerDto {
     private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
